f375e5611eaa23055be61e616ce7041108d65e2a
holy_nova_mac <holy_nova@126.com>
2018-11-11 16:28:34 +0800

    test calender component

--- src/routes/CalendarScreen.js
+++ src/routes/CalendarScreen.js
@@ -13,15 +13,43 @@ class CalendarScreen extends Component {
     super(props)
 
     this.onDayPress = this.onDayPress.bind(this)
+    this.onMonthChange = this.onMonthChange.bind(this)
   }
 
   onDayPress(dateObj) {
     log(JSON.stringify(dateObj, null, 2))
   }
 
+  onMonthChange(data) {
+    log(data.month)
+  }
+
   render() {
     const calendarProps = {
-      onDayPress: this.onDayPress
+      onDayPress: this.onDayPress,
+      onMonthChange: this.onMonthChange,
+      markedDates: {
+        // '2018-11-20': { textColor: 'green' },
+        '2018-11-22': { startingDay: true, color: 'skyblue' },
+        '2018-11-23': { endingDay: true, color: 'skyblue' },
+        // '2018-11-04': { disabled: true, startingDay: true, color: 'green', endingDay: true }
+
+        // '2018-11-1': {
+        //   startingDay: true,
+        //   // color: 'green',
+        //   // textColor: 'red',
+        //   selected: true
+        // },
+        // '2018-11-3': {
+        //   endingDay: true,
+        //   // color: 'green'
+        // },
+        // '2018-11-16': { selected: true, marked: true, selectedColor: 'red' },
+        // '2018-11-17': { marked: true },
+        // '2018-11-18': { marked: true, dotColor: 'red', activeOpacity: 0 },
+        // '2018-11-19': { disabled: true, disableTouchEvent: true }
+      },
+      markingType: "period",
     }
     return (
       <View style={styles.container} >
